50f91a29bfa8e938e3ff293b897d18b96b048090
Daniel Stanecki <dstanecki@hawk.iit.edu>
2025-06-24 16:12:25 -0500

    Standalone import-data.sql and mariadb Dockerfile

---
+++ data/import-data.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS ZillowHomeValueForecast;
+    USE ZillowHomeValueForecast;
+
+    CREATE TABLE forecast (
+        RegionID INT,
+        SizeRank INT,
+        RegionName VARCHAR(50),
+        RegionType VARCHAR(50),
+        StateName VARCHAR(50),
+        St VARCHAR(50),
+        City VARCHAR(50),
+        Metro VARCHAR(50),
+        CountyName VARCHAR(50),
+        BaseDate DATE,
+        `2025-06-30` DECIMAL(4, 2),
+        `2025-08-31` DECIMAL(4, 2),
+        `2026-05-31` DECIMAL(4, 2)
+    );
+
+    LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/data.csv'
+    INTO TABLE forecast
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS;
