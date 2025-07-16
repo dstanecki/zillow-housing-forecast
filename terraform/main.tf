e590ac37c7dd4c512a7c05394eb358571b0faf2a
dstanecki <staneckidaniel@gmail.com>
2025-07-15 19:51:28 -0500

    Add ArgoCD in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,20 @@ provider "google" {
   region  = var.region
 }
 
+provider "helm" {
+  kubernetes = {
+    host                   = google_container_cluster.zhf_cluster.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.zhf_cluster.master_auth[0].cluster_ca_certificate)
+  }
+}
+
 resource "google_compute_network" "zhf_network" {
   name = "zhf-network"
 
   auto_create_subnetworks  = false
   enable_ula_internal_ipv6 = true
 }
-
 
 resource "google_compute_subnetwork" "zhf_subnetwork" {
   name = "zhf-subnetwork"
@@ -52,4 +59,17 @@ resource "google_container_cluster" "zhf_cluster" {
   # Set `deletion_protection` to `true` will ensure that one cannot
   # accidentally delete this instance by use of Terraform.
   deletion_protection = false
+}
+
+data "google_client_config" "default" {}
+
+# Install ArgoCD
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "8.1.3" # check latest: https://artifacthub.io/packages/helm/argo/argo-cd
+  create_namespace = true
+  values = [file("../infra/argocd/values.yaml")]
 }
