4844939580d90276db705ae2c45c32f86a869ba5
dstanecki <staneckidaniel@gmail.com>
2025-07-23 20:33:34 -0500

    Add new vars

--- terraform/workloads/variables.tf
+++ terraform/workloads/variables.tf
@@ -11,3 +11,29 @@ variable "terraform_service_account_email" {
   type = string
   default = "zhf-terraform@zhf-project-1752025280949.iam.gserviceaccount.com"
 }
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "redis_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "azure_ai_openapi_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "recaptcha_secret_key_dev" {
+  type      = string
+  sensitive = true
+}
+
+variable "recaptcha_secret_key_prod" {
+  type      = string
+  sensitive = true
+}
+
